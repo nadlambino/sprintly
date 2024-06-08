847b0f171b87866350c47aa3a75c997a6ba34b1c
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2024-06-08 11:07:37 +0800

    Rollback changes from navlink and just use inertia's link component

--- resources/js/Pages/Tasks/SubMenu.vue
+++ resources/js/Pages/Tasks/SubMenu.vue
@@ -1,16 +1,16 @@
 <script setup>
 import PrimaryButton from '@/Components/PrimaryButton.vue';
 import AddIcon from '../../Icons/AddIcon.vue';
-import NavLink from '@/Components/NavLink.vue';
+import { Link } from '@inertiajs/vue3';
 </script>
 
 <template>
     <div class="flex gap-5">
-        <NavLink :href="route('tasks.create')" unstyled>
+        <Link :href="route('tasks.create')">
             <PrimaryButton class="flex gap-1 justify-center items-center">
                 <AddIcon class="text-white" />
                 <span>Create</span>
             </PrimaryButton>
-        </NavLink>
+        </Link>
     </div>
 </template>
