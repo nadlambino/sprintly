525d8891a5345438089bac6a6ff9ff1c69b0f861
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2024-06-09 18:14:28 +0800

    Mobile UI for trashed page

--- resources/js/Pages/Tasks/Trashed.vue
+++ resources/js/Pages/Tasks/Trashed.vue
@@ -25,7 +25,7 @@ const headers = [
             <SubMenu :trashed="false" :filterable="true" :statuses="statuses" />
         </template>
 
-        <div class="py-5">
+        <div class="p-5">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8 pb-5">
                 <h1 class="text-lg font-bold text-muted">Trashed</h1>
             </div>
