569790179a5944a3a3316e286e76f10c2d86e1af
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2024-06-09 16:27:23 +0800

    Redesign button dropdown

--- resources/js/Components/ButtonDropdown.vue
+++ resources/js/Components/ButtonDropdown.vue
@@ -3,7 +3,6 @@ import RadioInput from '@/Components/RadioInput.vue';
 import InputLabel from '@/Components/InputLabel.vue';
 import { ref } from 'vue';
 import { onClickOutside } from '@vueuse/core'
-import SecondaryButton from './SecondaryButton.vue';
 
 const props = defineProps({
     label: String,
@@ -19,20 +18,20 @@ onClickOutside(target, () => showSort.value = false);
 
 <template>
     <div ref="target" class="relative">
-        <SecondaryButton class="h-10" @click="showSort = !showSort" id="dropdownRadioButton" data-dropdown-toggle="dropdownDefaultRadio">
+        <button class="focus:ring-2 focus:ring-offset-2 focus:ring-gray-200 bg-secondary text-[10px] rounded-md flex justify-between items-center px-2 font-bold text-gray-700 uppercase h-10" @click="showSort = !showSort" id="dropdownRadioButton" data-dropdown-toggle="dropdownDefaultRadio">
             {{ label }}
             <svg class="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
                 <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 1 4 4 4-4"/>
             </svg>
-        </SecondaryButton>
+        </button>
 
         <!-- Dropdown menu -->
-        <div v-show="showSort" id="dropdownDefaultRadio" class="z-10 w-48 bg-white divide-y divide-gray-100 rounded-lg shadow-lg absolute right-0 top-12">
-            <ul class="py-1 text-sm text-gray-700" aria-labelledby="dropdownRadioButton">
+        <div v-show="showSort" id="dropdownDefaultRadio" class="z-10 w-48 bg-white divide-y divide-gray-100 rounded-lg shadow-xl absolute right-0 top-12">
+            <ul class="py-1 text-xs" aria-labelledby="dropdownRadioButton">
                 <li v-for="option in options" :key="option.value">
                     <div class="flex items-center">
                         <RadioInput class="hidden" :id="option.value" :value="option.value" v-model="selected" name="sort" />
-                        <InputLabel :for="option.value" :value="option.label" class="capitalize cursor-pointer w-full hover:bg-secondary p-1"/>
+                        <InputLabel :for="option.value" :value="option.label" class="capitalize cursor-pointer w-full hover:bg-secondary p-2" :class="{ 'bg-secondary': selected === option.value }"/>
                     </div>
                 </li>
             </ul>
