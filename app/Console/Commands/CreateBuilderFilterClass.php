9eb049df16ad4b4c03a861170ea6d1c68ed74702
Ronald Lambino <50892397+nadlambino@users.noreply.github.com>
2024-06-25 14:11:08 +0800

    Create CreateBuilderFilter command

---
+++ app/Console/Commands/CreateBuilderFilterClass.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\GeneratorCommand;
+
+class CreateBuilderFilterClass extends GeneratorCommand
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'make:builder-filter {name}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Creates a builder filter class.';
+
+    protected function getStub()
+    {
+        return storage_path('stubs\builder\filter.stub');
+    }
+
+    protected function getDefaultNamespace($rootNamespace)
+    {
+        return $rootNamespace . '\QueryBuilders';
+    }
+}
